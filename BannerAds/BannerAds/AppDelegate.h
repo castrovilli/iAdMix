959cb28b45056c013a88e45b279a7f4b494728c5
grharon <grharon@gmail.con>
2014-02-12 23:52:52 +0800

    BannerAdds 1 Commit

---
+++ BannerAds/BannerAds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BannerAds
+//
+//  Created by grharon on 2/12/14.
+//  Copyright (c) 2014 limalogy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BannerAds/BannerAdsTests/BannerAdsTests.m
@@ -0,0 +1,34 @@
+//
+//  BannerAdsTests.m
+//  BannerAdsTests
+//
+//  Created by grharon on 2/12/14.
+//  Copyright (c) 2014 limalogy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BannerAdsTests : XCTestCase
+
+@end
+
+@implementation BannerAdsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
