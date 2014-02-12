160661f8fa862cea05f760f552b6b29b68652254
grharon <grharon@gmail.con>
2014-02-12 23:58:06 +0800

    Interstitial Ads 1 Commit

---
+++ InterstitialAds/InterstitialAds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InterstitialAds
+//
+//  Created by grharon on 2/12/14.
+//  Copyright (c) 2014 limalogy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
